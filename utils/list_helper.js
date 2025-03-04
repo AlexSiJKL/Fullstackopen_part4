2f467d5d52fa6a218ff03a9f7f24d63b289c7f46
AlexJKL <alexandr_sidorenko@icloud.com>
2025-03-04 14:20:04 +0200

    4.5*: Helper Functions and Unit Tests, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,10 +6,22 @@ const totalLikes = (blogs) => {
   let totalLikes = 0
   blogs.forEach(blog => {
     totalLikes += blog.likes
-  });
+  })
   return totalLikes
 }
 
+const favoriteBlog = (blogs) => {
+  let maxLikes = 0
+  let favoriteBlogIndex = 0
+  blogs.forEach((blog, index) => {
+    if (blog.likes >= maxLikes) {
+      maxLikes = blog.likes
+      favoriteBlogIndex = index
+    }
+  })
+  return blogs[favoriteBlogIndex]
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
